e32ae6ce4f5707336a42ec74d4ad560eea4281e2
Matt Arcidy <marcidy@gmail.com>
2018-12-25 13:02:24 -0800

    trying to get images to work

--- ordi/raspi/ordi.py
+++ ordi/raspi/ordi.py
@@ -5,16 +5,29 @@ from pyspacelib.ft.ft_controller import (
     FTImage,
 )
 from kivy.app import App
+from kivy.config import Config
+from kivy.core.window import Window
 from kivy.graphics import Color, Ellipse
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.config import Config
+from kivy.uix.image import Image, AsyncImage
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
 
-Config.set('graphics', 'width', '640')
-Config.set('graphics', 'height', '480')
+# Config.set('graphics', 'width', '480')
+# Config.set('graphics', 'height', '320')
+# Config.set('graphics', 'fullscreen', 'auto')
+# Config.set('graphics', 'window_state', 'maximized')
+# Window.fullscreen = 'auto'
 
 
-class FlaschenTaschenViewer(BoxLayout):
+class OrdiBooth(BoxLayout):
+
+        source = '/home/pi/Projects/nb_ordi/ordi/rasp/water.jpg'
+
+
+class FlaschenTaschenViewer(Widget):
 
     ftc = FTController()
 
@@ -26,13 +39,14 @@ class FlaschenTaschenViewer(BoxLayout):
         height = ft.height
         width = ft.width
         grid = self.ftc.grid
+        bottle_size = 12
 
         with wid.canvas:
             for y in range(width):
                 for x in range(height):
                     r, g, b = grid[height-(x+1)][y]
                     Color(r/255, g/255, b/255)
-                    Ellipse(pos=(y*20, x*20), size=(20, 20))
+                    Ellipse(pos=(y*bottle_size, x*bottle_size), size=(bottle_size, bottle_size))
 
     def reset(self, wid):
         wid.canvas.clear()
@@ -40,19 +54,19 @@ class FlaschenTaschenViewer(BoxLayout):
     def show(self):
         self.ftc.show()
 
+#     def build(self):d
+#         root = Builder.load_file("ordi.kv")
+#         return root
+
+
+class OrdiApp(App):
+
     def build(self):
-        root = Builder.load_file("ordi.kv")
-        return root
-
-
-class FlaschenTaschenApp(App):
-
-    def build(self):
-        Builder.load_file("./ordi.kv")
-        return FlaschenTaschenViewer()
+        # Builder.load_file("./ordi.kv")
+        return OrdiBooth()
 
 
 if __name__ in ("__main__", "__android__"):
 
-    app = FlaschenTaschenApp()
+    app = OrdiApp()
     app.run()
